543c82489cd7867a15f78641032e03432bcef8da
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-08-11 21:11:12 +0800

    Add gulpfile

---
+++ 4/gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('serve', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default', ['sass', 'sass:watch', 'serve']);
