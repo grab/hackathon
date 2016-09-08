4d52a9f6324d575bf4e3c0f2d3be4d3dfb80e6a3
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-09-09 01:54:44 +0800

    Change presentation timing

--- 4.0/js/countdown.js
+++ 4.0/js/countdown.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date("September 9, 2016 10:00:00");
+var deadline = new Date("September 9, 2016 12:00:00");
 initializeClock('countdown', deadline);
