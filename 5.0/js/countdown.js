a51d7a660aefc65128b17384e1b1ed7197a8f664
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-05-08 12:41:40 +0800

    Update site

--- 5.0/js/countdown.js
+++ 5.0/js/countdown.js
@@ -40,5 +40,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date("May 9, 2017 10:00:00");
+var deadline = new Date("May 9, 2017 11:00:00");
 initializeClock('countdown', deadline);
